9d438cf322f9335140e702d9805b15ac1b5efea8
Ali Rudnick <ali.rudnick@yahoo.com>
2022-11-13 18:19:08 -0600

    Completed JS, added styling and README

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,22 +1,62 @@
 
-//global variables
-  var currentDay = document.getElementById("currentDay");
-  var day = dayjs().format();
-  var saveBtn = document.querySelectorAll(".saveBtn");
-  console.log(saveBtn);
-
-
-  currentDay.textContent = day;
-for (i = 0; i < saveBtn.length; i++) {
-  saveBtn[i].addEventListener("click", function() {
-    var text = this.previousElementSibling.value
-    console.log(text);
-    var hour = this.parentElement.id 
-    localStorage.setItem(hour, text)
-  })
+$(document).ready(function () {
   
+  //Current day & time
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
+
+//saveBtn 
+  $(".saveBtn").on("click", function () {
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    //save to local storage
+    localStorage.setItem(time, text);
+  })
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour18 .description").val(localStorage.getItem("hour18"));
+    $("#hour19 .description").val(localStorage.getItem("hour19"));
+
+function hourTracker() {
+  //get current number of hours
+  var currentHour = moment().hour();
+
+  // loop over blocks
+  $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+      console.log( blockHour, currentHour)
+
+      //check if we've moved past this time
+      if (blockHour < currentHour) {
+          $(this).addClass("past");
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+      }
+      else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+          $(this).removeClass("future");
+      }
+      else {
+          $(this).removeClass("present");
+          $(this).removeClass("past");
+          $(this).addClass("future");
+      }
+  })
 }
+hourTracker();
+
+})
   
 
 
